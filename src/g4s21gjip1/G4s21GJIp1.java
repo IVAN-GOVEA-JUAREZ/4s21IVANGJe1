0630c65306c392874b1f527a653586743768d2ec
Space <Space@192.168.1.67>
2022-04-06 10:45:05 -0500

    G4s21GJIp1

---
+++ src/g4s21gjip1/G4s21GJIp1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package g4s21gjip1;
+
+/**
+ *
+ * @author SpaceWolf
+ */
+public class G4s21GJIp1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new FrmPrincipal().setVisible(true);
+        //new FrmSegundo().setVisible(true);
+    }
+    
+}
